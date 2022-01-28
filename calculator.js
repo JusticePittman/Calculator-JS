d96a4200069e86d3f968f1235d702b0dbf2ecf62
Justice Pittman <justice.a.pittman@gmail.com>
2022-01-28 07:21:17 -0500

    Button events added. No functions written yet

--- calculator.js
+++ calculator.js
@@ -1,32 +1,53 @@
-const add = function (num1, num2) {
-    return num1 + num2;
-};
-const subtract = function (num1, num2) {
-    return num1 - num2;
-};
-const multiply = function (num1, num2) {
-    return num1 * num2;
-};
-const divide = function (num1, num2) {
-    return num1 / num2;
-};
+const displayDefault = null;
+const displayVal = document.querySelector(".display");
+displayVal.textContent = displayDefault;
 
-const operate = function (num1, num2, operator) {
+let num1;
+let num2;
+let operator = null;
+
+//Button Selectors
+const numButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector(".equals");
+const miscButtons = document.querySelectorAll(".misc");
+
+numButtons.forEach((button) =>
+    button.addEventListener("click", () => setNum(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+    button.addEventListener("click", () => setOperator(button.textContent))
+);
+
+equalButton.addEventListener("click", operate());
+
+//Basic Math Functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
             return add(num1, num2);
-            break;
         case "-":
             return subtract(num1, num2);
-            break;
         case "*":
             return multiply(num1, num2);
-            break;
         case "/":
             return num2 === 0 ? null : divide(num1, num2);
-            break;
 
         default:
             break;
     }
-};
+}
